01f93a4e509898f01de5710719a3a133aa8c9383
joseismaelrj <jose.nascimento@mercadolivre.com>
2022-01-18 18:32:59 -0300

    Criando sengundo commit

---
+++ Documents/MeusProjetos/AtividadeJPATeste/src/main/java/com/example/atividadejpateste/AtividadeJpaTesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividadejpateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeJpaTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtividadeJpaTesteApplication.class, args);
+    }
+
+}
